5e0a70d467a435bf2cd028674f93d111f8a5744c
Christelmarita <christelmarita.oe@gmail.com>
2023-06-05 18:34:27 +0200

    logo changes + some more

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     document.querySelector(".dark-underlay").classList.toggle("show-underlay");
   });
+
+// SCROLL EFFECT ON ABOUT IMAGE
+
+function scrollFadeIn() {
+    var breakElement = document.querySelector('.abt-content img');
+    var breakElementOffset = breakElement.offsetTop;
+    var windowHeight = window.innerHeight;
+
+    function useScroll() {
+        var scrollTop = window.scrollY;
+
+        if (scrollTop + (windowHeight * 0.8) > breakElementOffset) {
+            breakElement.classList.add('visible');
+        } else {
+            breakElement.classList.remove('visible');
+        }
+    }
+
+    window.addEventListener('scroll', useScroll);
+}
+
+scrollFadeIn();
+
