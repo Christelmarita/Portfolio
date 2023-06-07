53ccec59e5587f038809a22d7b4c6ab4b708d5b0
Christelmarita <christelmarita.oe@gmail.com>
2023-06-07 23:08:49 +0200

    media

--- js/script.js
+++ js/script.js
@@ -40,22 +40,40 @@ document.addEventListener("DOMContentLoaded", function () {
   
     images.forEach(function (image) {
       image.addEventListener("click", function () {
-        if (window.innerWidth > 700) {
-          modal.style.display = "block";
-          img.src = this.src;
-        }
+        modal.style.display = "block";
+        img.src = this.src;
       });
     });
   
     var closeBtn = document.getElementsByClassName("close")[0];
     modal.addEventListener("click", function (event) {
-      if (
-        event.target === modal ||
-        event.target === closeBtn ||
-        window.innerWidth <= 700
-      ) {
+      if (event.target === modal || event.target === closeBtn) {
         modal.style.display = "none";
       }
     });
+});
+
+// SCROLL TO SECTIONS
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link");
+    for (const link of navLinks) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const target = this.getAttribute("href");
+        const offset = document.querySelector(".navbar").offsetHeight;
+        const targetPosition = document.querySelector(target).offsetTop - offset;
+  
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth"
+        });
+
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        document.querySelector(".dark-underlay").classList.remove("show-underlay");
+
+      });
+    }
   });
   
